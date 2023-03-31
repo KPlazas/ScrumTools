da083809475fe758a155ddb056562934ffe3e63a
Jhonatan1523 <jhonatanjyyplm@gmail.com>
2023-03-31 07:31:30 -0500

    update

--- frontscrumtools/src/pages/Home.js
+++ frontscrumtools/src/pages/Home.js
@@ -1,4 +1,8 @@
-const Home =()=>{
-    return <h1>Home</h1>
+import { Fragment } from "react";
+
+function Home (){
+    return (<Fragment>
+        <h1>Home</h1>
+    </Fragment>);
 }
 export default Home;
