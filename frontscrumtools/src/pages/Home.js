22ff3f9369bf8f1c51be9ebcb708db2f39d5ee50
Jhonatan1523 <jhonatanjyyplm@gmail.com>
2023-04-26 21:23:21 -0500

    Front

--- frontscrumtools/src/pages/Home.js
+++ frontscrumtools/src/pages/Home.js
@@ -2,7 +2,10 @@ import { Fragment } from "react";
 
 function Home (){
     return (<Fragment>
-        <h1>Home</h1>
+        <h1 style={{ color: 'white' }}><marquee>Bienvenido a Agile Tools bebe :v</marquee></h1>
+        <img style={{ width: '300px', height: '400px'  ,display: 'block', margin: 'auto'}}
+        src="https://i.pinimg.com/474x/87/0f/68/870f68855f570b082cdeead311445500.jpg" ></img>
+        
     </Fragment>);
 }
 export default Home;
